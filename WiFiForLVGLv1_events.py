239c6e3233267f13bf4de52b135ebc6d2f976e17
G2EJ-IC <IC.G2EJ@outlook.com>
2024-02-14 00:10:02 -0500

    Initial commit

---
+++ WiFiForLVGLv1_events.py
@@ -0,0 +1,16 @@
+
+def ShowKeyBoard(event_struct):
+    return
+
+
+def HideKeyBoard(event_struct):
+    return
+
+
+def ConnectToRouter(event_struct):
+    return
+
+
+def ReScanWiFiSSID(event_struct):
+    return
+
